5a7089e3745fd110a9aa46b2d6c7813227a0c613
Jonas Schumacher <github735@jonasschumacher.de>
2024-08-01 12:54:35 +0200

    Add aqua and fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using SwaggerMarkdown
 using Test
+using Aqua
 
 const GET = "get"
 const PUT = "put"
@@ -8,6 +9,10 @@ const POST = "post"
 const ROOT = dirname(dirname(@__FILE__))
 const TEST_ROOT = dirname(@__FILE__)
 const SPECS_PATH = joinpath(TEST_ROOT, "specs")
+
+@testset "Aqua" begin
+  Aqua.test_all(SwaggerMarkdown)
+end
 
 include("file_build_tests.jl")
 include("macro_build_tests.jl")
